e03707da49491c25f14c0b3540c554c6528bc50c
Wanderson Nunes <87546860+nunespc@users.noreply.github.com>
2022-01-28 00:15:53 -0300

    Add files via upload

---
+++ projeto_basico_notas/src/pessoa/Aluno.java
@@ -0,0 +1,20 @@
+package pessoa;
+
+public class Aluno {
+	public String nome = "Laura";
+
+	public Aluno(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
